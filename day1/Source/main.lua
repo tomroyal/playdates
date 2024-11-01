149f8e8666b458ff0084bd5d99045c67215bc7c4
Tom Royal <tom@tomroyal.com>
2024-11-01 16:08:33 +0000

    day 1

---
+++ day1/Source/main.lua
@@ -0,0 +1,68 @@
+-- this is a comment
+-- import core libraries
+
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local gfx <const> = playdate.graphics
+
+-- Here's our player sprite declaration. We'll scope it to this file because
+-- several functions need to access it.
+
+local playerSprite = nil
+
+-- A function to set up our game environment.
+
+function myGameSetUp()
+
+    -- Set up the player sprite.
+
+    local playerImage = gfx.image.new("Images/happy.png")
+    assert( playerImage ) -- make sure the image was where we thought
+
+    playerSprite = gfx.sprite.new( playerImage )
+    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
+    playerSprite:add() -- This is critical!
+
+end
+
+-- Now we'll call the function above to configure our game.
+-- After this runs (it just runs once), nearly everything will be
+-- controlled by the OS calling `playdate.update()` 30 times a second.
+
+myGameSetUp()
+
+-- `playdate.update()` is the heart of every Playdate game.
+-- This function is called right before every frame is drawn onscreen.
+-- Use this function to poll input, run game logic, and move sprites.
+
+function playdate.update()
+
+    -- Poll the d-pad and move our player accordingly.
+    -- (There are multiple ways to read the d-pad; this is the simplest.)
+    -- Note that it is possible for more than one of these directions
+    -- to be pressed at once, if the user is pressing diagonally.
+
+    if playdate.buttonIsPressed( playdate.kButtonUp ) then
+        playerSprite:moveBy( 0, -2 )
+    end
+    if playdate.buttonIsPressed( playdate.kButtonRight ) then
+        playerSprite:moveBy( 2, 0 )
+    end
+    if playdate.buttonIsPressed( playdate.kButtonDown ) then
+        playerSprite:moveBy( 0, 2 )
+    end
+    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+        playerSprite:moveBy( -2, 0 )
+    end
+
+    -- Call the functions below in playdate.update() to draw sprites and keep
+    -- timers updated. (We aren't using timers in this example, but in most
+    -- average-complexity games, you will.)
+
+    gfx.sprite.update()
+    playdate.timer.updateTimers()
+
+end
